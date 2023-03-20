87057f45137e79f8199005a200e0d2f9583f5f9e
Michael Vogt <mvo@ubuntu.com>
2023-03-20 11:45:45 +0100

    epoll,prompting: fix test import from cerberus->snapd
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>

--- prompting/notifier/notifier.go
+++ prompting/notifier/notifier.go
@@ -8,8 +8,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/snapcore/cerberus/apparmor"
-	"github.com/snapcore/cerberus/epoll"
+	"github.com/snapcore/snapd/prompting/apparmor"
+	"github.com/snapcore/snapd/osutil/epoll"
 )
 
 // Notifier contains low-level components for receiving notification requests
