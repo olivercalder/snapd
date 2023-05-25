canonical/x-go

randutil: add unittests for RandomKernelUUID()

randutil/crypto_test.go
@@ -21,6 +21,9 @@ package randutil_test
 
 import (
 	"encoding/base64"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
 	. "gopkg.in/check.v1"
 
@@ -44,4 +47,70 @@ func (s *cryptoRandutilSuite) TestCryptoToken(c *C) {
 	b, err := base64.RawURLEncoding.DecodeString(x)
 	c.Assert(err, IsNil)
 	c.Check(b, HasLen, 5)
+}
+
+var (
+	kernelTestUUID = "1031319a-b661-4c01-aafa-6def8a118944"
+)
+
+func (s *cryptoRandutilSuite) TestRandomKernelUUIDNoFile(c *C) {
+	uuidPath := filepath.Join(c.MkDir(), "no-file")
+	defer randutil.MockKernelUUIDPath(uuidPath)()
+
+	value, err := randutil.RandomKernelUUID()
+	c.Check(value, Equals, "")
+	c.Check(err, ErrorMatches,
+		"cannot access kernel generated uuid:"+
+			".*no-file: no such file or directory")
+}
+
+func (s *cryptoRandutilSuite) TestRandomKernelUUIDNoPerm(c *C) {
+	if os.Getuid() == 0 {
+		c.Skip("Permission tests will not work when user is root")
+	}
+
+	uuidPath := filepath.Join(c.MkDir(), "no-perm")
+	defer randutil.MockKernelUUIDPath(uuidPath)()
+
+	err := ioutil.WriteFile(uuidPath, []byte(kernelTestUUID), 0)
+	c.Assert(err, IsNil)
+
+	value, err := randutil.RandomKernelUUID()
+	c.Check(value, Equals, "")
+	c.Assert(err, ErrorMatches,
+		"cannot access kernel generated uuid:"+
+			".*no-perm: permission denied")
+}
+
+func (s *cryptoRandutilSuite) TestRandomKernelUUID(c *C) {
+	for _, uuid := range []string{
+		kernelTestUUID,
+		" \t\n " + kernelTestUUID + " \n\t\r\n",
+	} {
+		// Create new path on each iteration because we cannot
+		// reuse previous path to read-only (0444) file.
+		uuidPath := filepath.Join(c.MkDir(), "uuid")
+		defer randutil.MockKernelUUIDPath(uuidPath)()
+
+		err := ioutil.WriteFile(uuidPath, []byte(uuid), 0444)
+		c.Assert(err, IsNil)
+
+		value, err := randutil.RandomKernelUUID()
+		c.Check(value, Equals, kernelTestUUID)
+		c.Assert(err, IsNil)
+	}
+}
+
+func (s *cryptoRandutilSuite) TestRandomKernelUUIDReal(c *C) {
+	if _, err := os.Stat(randutil.KernelUUIDPath); err != nil {
+		c.Skip("Kernel UUID procfs file is not accessible in the current test environment")
+	}
+
+	value, err := randutil.RandomKernelUUID()
+	c.Check(value, Not(Equals), "")
+	// https://www.rfc-editor.org/rfc/rfc4122#section-3
+	// We are not testing the kernel here, so minimal check:
+	// UUID should be 36 bytes in length exactly.
+	c.Check(value, HasLen, 36)
+	c.Assert(err, IsNil)
 }

olivercalder/snapd

randutil/crypto_test.go
@@ -21,6 +21,9 @@ package randutil_test
 
 import (
 	"encoding/base64"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
 	. "gopkg.in/check.v1"
 
@@ -44,4 +47,70 @@ func (s *cryptoRandutilSuite) TestCryptoToken(c *C) {
 	b, err := base64.RawURLEncoding.DecodeString(x)
 	c.Assert(err, IsNil)
 	c.Check(b, HasLen, 5)
+}
+
+var (
+	kernelTestUUID = "1031319a-b661-4c01-aafa-6def8a118944"
+)
+
+func (s *cryptoRandutilSuite) TestRandomKernelUUIDNoFile(c *C) {
+	uuidPath := filepath.Join(c.MkDir(), "no-file")
+	defer randutil.MockKernelUUIDPath(uuidPath)()
+
+	value, err := randutil.RandomKernelUUID()
+	c.Check(value, Equals, "")
+	c.Check(err, ErrorMatches,
+		"cannot read kernel generated uuid:"+
+			".*no-file: no such file or directory")
+}
+
+func (s *cryptoRandutilSuite) TestRandomKernelUUIDNoPerm(c *C) {
+	if os.Getuid() == 0 {
+		c.Skip("Permission tests will not work when user is root")
+	}
+
+	uuidPath := filepath.Join(c.MkDir(), "no-perm")
+	defer randutil.MockKernelUUIDPath(uuidPath)()
+
+	err := ioutil.WriteFile(uuidPath, []byte(kernelTestUUID), 0)
+	c.Assert(err, IsNil)
+
+	value, err := randutil.RandomKernelUUID()
+	c.Check(value, Equals, "")
+	c.Assert(err, ErrorMatches,
+		"cannot read kernel generated uuid:"+
+			".*no-perm: permission denied")
+}
+
+func (s *cryptoRandutilSuite) TestRandomKernelUUID(c *C) {
+	for _, uuid := range []string{
+		kernelTestUUID,
+		" \t\n " + kernelTestUUID + " \n\t\r\n",
+	} {
+		// Create new path on each iteration because we cannot
+		// reuse previous path to read-only (0444) file.
+		uuidPath := filepath.Join(c.MkDir(), "uuid")
+		defer randutil.MockKernelUUIDPath(uuidPath)()
+
+		err := ioutil.WriteFile(uuidPath, []byte(uuid), 0444)
+		c.Assert(err, IsNil)
+
+		value, err := randutil.RandomKernelUUID()
+		c.Check(value, Equals, kernelTestUUID)
+		c.Assert(err, IsNil)
+	}
+}
+
+func (s *cryptoRandutilSuite) TestRandomKernelUUIDReal(c *C) {
+	if _, err := os.Stat(randutil.KernelUUIDPath); err != nil {
+		c.Skip("Kernel UUID procfs file is not accessible in the current test environment")
+	}
+
+	value, err := randutil.RandomKernelUUID()
+	c.Check(value, Not(Equals), "")
+	// https://www.rfc-editor.org/rfc/rfc4122#section-3
+	// We are not testing the kernel here, so minimal check:
+	// UUID should be 36 bytes in length exactly.
+	c.Check(value, HasLen, 36)
+	c.Assert(err, IsNil)
 }