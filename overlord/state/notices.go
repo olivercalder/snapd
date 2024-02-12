2bb5480b535a868e94423d644ddf3cb77b52330a
Oliver Calder <oliver.calder@canonical.com>
2024-02-12 11:43:11 -0600

    o/{state,i/apparmorprompting}: rename prompting notice types
    Rename "interfaces-prompting-request" to "interfaces-requests-prompt".
    Rename "interfaces-prompting-rule-update" to "interfaces-requests-rule-update".
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>

--- overlord/state/notices.go
+++ overlord/state/notices.go
@@ -200,19 +200,19 @@ const (
 	// Recorded whenever an auto-refresh is inhibited for one or more snaps.
 	RefreshInhibitNotice NoticeType = "refresh-inhibit"
 
-	// Recorded whenever a prompt request is created or resolved. The key for
-	// interfaces-prompting-request notices is the request ID.
-	PromptingRequestNotice NoticeType = "interfaces-prompting-request"
+	// Recorded whenever a request prompt is created or resolved. The key for
+	// interfaces-requests-prompt notices is the request prompt ID.
+	RequestsPromptNotice NoticeType = "interfaces-requests-prompt"
 
-	// Recorded whenever a prompting rule is created, modified, deleted, or
-	// expired. The key for interfaces-prompting-rule-update notices is the
+	// Recorded whenever a request rule is created, modified, deleted, or
+	// expired. The key for interfaces-requests-rule-update notices is the
 	// rule ID.
-	PromptingRuleUpdateNotice NoticeType = "interfaces-prompting-rule-update"
+	RequestsRuleUpdateNotice NoticeType = "interfaces-requests-rule-update"
 )
 
 func (t NoticeType) Valid() bool {
 	switch t {
-	case ChangeUpdateNotice, WarningNotice, RefreshInhibitNotice, PromptingRequestNotice, PromptingRuleUpdateNotice:
+	case ChangeUpdateNotice, WarningNotice, RefreshInhibitNotice, RequestsPromptNotice, RequestsRuleUpdateNotice:
 		return true
 	}
 	return false
