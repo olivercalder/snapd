e6819875edde3ccb46d8b8c149daa68481b1bead
Oliver Calder <oliver.calder@canonical.com>
2024-06-05 22:54:25 -0500

    i/prompting/requestprompts: include reason in notice data when prompt resolved
    A prompt may only be resolved once, and the notice recorded when that
    prompt is resolved is the final notice which will be associated with
    that prompt ID. Therefore, it is safe to include notice data without
    risk of signal loss.
    
    Currently, prompt clients need to query the prompting API in order to
    check whether an outstanding prompt was modified (due to some but not
    all of its permissions being satisfied by a new rule) or fully resolved,
    either due to a direct reply, a new rule satisfying all remaining
    permissions, or the prompt DB being closed.
    
    Since the notice recorded when a prompt is resolved is the final notice
    for that prompt, we can instead include the "resolved" key in the
    notice data to indicate to the client that the prompt has been resolved
    without the need to query the prompting API. And the value for the
    "resolved" key can be "replied", "satisfied", or "cancelled", giving
    additional information to the prompt client about why the prompt was
    resolved.
    
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>

--- interfaces/prompting/requestprompts/requestprompts_test.go
+++ interfaces/prompting/requestprompts/requestprompts_test.go
@@ -24,6 +24,7 @@ import (
 	"os"
 	"path/filepath"
 	"sort"
+	"strings"
 	"testing"
 	"time"
 
@@ -39,10 +40,31 @@ import (
 
 func Test(t *testing.T) { TestingT(t) }
 
+type noticeInfo struct {
+	promptID string
+	data     map[string]string
+}
+
+type noticeList []*noticeInfo
+
+func (l noticeList) Len() int {
+	return len(l)
+}
+
+func (l noticeList) Less(i, j int) bool {
+	return strings.Compare(l[i].promptID, l[j].promptID) < 0
+}
+
+func (l noticeList) Swap(i, j int) {
+	atI := l[i]
+	l[i] = l[j]
+	l[j] = atI
+}
+
 type requestpromptsSuite struct {
-	defaultNotifyPrompt func(userID uint32, promptID string) error
+	defaultNotifyPrompt func(userID uint32, promptID string, data map[string]string) error
 	defaultUser         uint32
-	noticePromptIDs     []string
+	promptNotices       noticeList
 
 	tmpdir string
 }
@@ -51,19 +73,23 @@ var _ = Suite(&requestpromptsSuite{})
 
 func (s *requestpromptsSuite) SetUpTest(c *C) {
 	s.defaultUser = 1000
-	s.defaultNotifyPrompt = func(userID uint32, promptID string) error {
+	s.defaultNotifyPrompt = func(userID uint32, promptID string, data map[string]string) error {
 		c.Check(userID, Equals, s.defaultUser)
-		s.noticePromptIDs = append(s.noticePromptIDs, promptID)
+		info := &noticeInfo{
+			promptID: promptID,
+			data:     data,
+		}
+		s.promptNotices = append(s.promptNotices, info)
 		return nil
 	}
-	s.noticePromptIDs = make([]string, 0)
+	s.promptNotices = make([]*noticeInfo, 0)
 	s.tmpdir = c.MkDir()
 	dirs.SetRootDir(s.tmpdir)
 	c.Assert(os.MkdirAll(dirs.SnapRunDir, 0700), IsNil)
 }
 
 func (s *requestpromptsSuite) TestNew(c *C) {
-	notifyPrompt := func(userID uint32, promptID string) error {
+	notifyPrompt := func(userID uint32, promptID string, data map[string]string) error {
 		c.Fatalf("unexpected notice with userID %d and ID %s", userID, promptID)
 		return nil
 	}
@@ -73,7 +99,7 @@ func (s *requestpromptsSuite) TestNew(c *C) {
 }
 
 func (s *requestpromptsSuite) TestLoadMaxID(c *C) {
-	notifyPrompt := func(userID uint32, promptID string) error {
+	notifyPrompt := func(userID uint32, promptID string, data map[string]string) error {
 		c.Fatalf("unexpected notice with userID %d and ID %s", userID, promptID)
 		return nil
 	}
@@ -194,7 +220,7 @@ func (s *requestpromptsSuite) TestAddOrMerge(c *C) {
 	after := time.Now()
 	c.Assert(merged, Equals, false)
 
-	s.checkNewNotices(c, []string{prompt1.ID})
+	s.checkNewNoticesSimple(c, []string{prompt1.ID}, nil)
 	c.Check(pdb.MaxID(), Equals, uint64(1))
 	s.checkWrittenMaxID(c, prompt1.ID)
 
@@ -203,7 +229,7 @@ func (s *requestpromptsSuite) TestAddOrMerge(c *C) {
 	c.Assert(prompt2, Equals, prompt1)
 
 	// Merged prompts should re-record notice
-	s.checkNewNotices(c, []string{prompt1.ID})
+	s.checkNewNoticesSimple(c, []string{prompt1.ID}, nil)
 	// Merged prompts should not advance the max ID
 	c.Check(pdb.MaxID(), Equals, uint64(1))
 	s.checkWrittenMaxID(c, prompt1.ID)
@@ -225,28 +251,48 @@ func (s *requestpromptsSuite) TestAddOrMerge(c *C) {
 	c.Check(storedPrompt, Equals, prompt1)
 
 	// Looking up prompt should not record notice
-	s.checkNewNotices(c, []string{})
+	s.checkNewNoticesSimple(c, []string{}, nil)
 
 	prompt3, merged := pdb.AddOrMerge(metadata, path, permissions, listenerReq3)
 	c.Check(merged, Equals, true)
 	c.Check(prompt3, Equals, prompt1)
 
 	// Merged prompts should re-record notice
-	s.checkNewNotices(c, []string{prompt1.ID})
+	s.checkNewNoticesSimple(c, []string{prompt1.ID}, nil)
 	// Merged prompts should not advance the max ID
 	c.Check(pdb.MaxID(), Equals, uint64(1))
 	s.checkWrittenMaxID(c, prompt1.ID)
 }
 
-func (s *requestpromptsSuite) checkNewNotices(c *C, expectedPromptIDs []string) {
-	c.Check(s.noticePromptIDs, DeepEquals, expectedPromptIDs)
-	s.noticePromptIDs = s.noticePromptIDs[:0]
+func (s *requestpromptsSuite) checkNewNoticesSimple(c *C, expectedPromptIDs []string, expectedData map[string]string) {
+	s.checkNewNotices(c, applyNotices(expectedPromptIDs, expectedData))
 }
 
-func (s *requestpromptsSuite) checkNewNoticesUnordered(c *C, expectedPromptIDs []string) {
-	sort.Strings(s.noticePromptIDs)
-	sort.Strings(expectedPromptIDs)
-	s.checkNewNotices(c, expectedPromptIDs)
+func applyNotices(expectedPromptIDs []string, expectedData map[string]string) noticeList {
+	expectedNotices := make(noticeList, len(expectedPromptIDs))
+	for i, id := range expectedPromptIDs {
+		info := &noticeInfo{
+			promptID: id,
+			data:     expectedData,
+		}
+		expectedNotices[i] = info
+	}
+	return expectedNotices
+}
+
+func (s *requestpromptsSuite) checkNewNotices(c *C, expectedNotices noticeList) {
+	c.Check(s.promptNotices, DeepEquals, expectedNotices)
+	s.promptNotices = s.promptNotices[:0]
+}
+
+func (s *requestpromptsSuite) checkNewNoticesUnorderedSimple(c *C, expectedPromptIDs []string, expectedData map[string]string) {
+	s.checkNewNoticesUnordered(c, applyNotices(expectedPromptIDs, expectedData))
+}
+
+func (s *requestpromptsSuite) checkNewNoticesUnordered(c *C, expectedNotices noticeList) {
+	sort.Sort(s.promptNotices)
+	sort.Sort(expectedNotices)
+	s.checkNewNotices(c, expectedNotices)
 }
 
 func (s *requestpromptsSuite) TestPromptWithIDErrors(c *C) {
@@ -271,7 +317,7 @@ func (s *requestpromptsSuite) TestPromptWithIDErrors(c *C) {
 	prompt, merged := pdb.AddOrMerge(metadata, path, permissions, listenerReq)
 	c.Check(merged, Equals, false)
 
-	s.checkNewNotices(c, []string{prompt.ID})
+	s.checkNewNoticesSimple(c, []string{prompt.ID}, nil)
 
 	result, err := pdb.PromptWithID(metadata.User, prompt.ID)
 	c.Check(err, IsNil)
@@ -286,7 +332,7 @@ func (s *requestpromptsSuite) TestPromptWithIDErrors(c *C) {
 	c.Check(result, IsNil)
 
 	// Looking up prompts (with or without errors) should not record notices
-	s.checkNewNotices(c, []string{})
+	s.checkNewNoticesSimple(c, []string{}, nil)
 }
 
 func (s *requestpromptsSuite) TestReply(c *C) {
@@ -316,14 +362,14 @@ func (s *requestpromptsSuite) TestReply(c *C) {
 		prompt1, merged := pdb.AddOrMerge(metadata, path, permissions, listenerReq1)
 		c.Check(merged, Equals, false)
 
-		s.checkNewNotices(c, []string{prompt1.ID})
+		s.checkNewNoticesSimple(c, []string{prompt1.ID}, nil)
 
 		prompt2, merged := pdb.AddOrMerge(metadata, path, permissions, listenerReq2)
 		c.Check(merged, Equals, true)
 		c.Check(prompt2, Equals, prompt1)
 
 		// Merged prompts should re-record notice
-		s.checkNewNotices(c, []string{prompt1.ID})
+		s.checkNewNoticesSimple(c, []string{prompt1.ID}, nil)
 
 		repliedPrompt, err := pdb.Reply(metadata.User, prompt1.ID, outcome)
 		c.Check(err, IsNil)
@@ -339,7 +385,8 @@ func (s *requestpromptsSuite) TestReply(c *C) {
 			c.Check(allowed, Equals, expected)
 		}
 
-		s.checkNewNotices(c, []string{repliedPrompt.ID})
+		expectedData := map[string]string{"resolved": "replied"}
+		s.checkNewNoticesSimple(c, []string{repliedPrompt.ID}, expectedData)
 	}
 }
 
@@ -379,7 +426,7 @@ func (s *requestpromptsSuite) TestReplyErrors(c *C) {
 	prompt, merged := pdb.AddOrMerge(metadata, path, permissions, listenerReq)
 	c.Check(merged, Equals, false)
 
-	s.checkNewNotices(c, []string{prompt.ID})
+	s.checkNewNoticesSimple(c, []string{prompt.ID}, nil)
 
 	outcome := prompting.OutcomeAllow
 
@@ -396,7 +443,7 @@ func (s *requestpromptsSuite) TestReplyErrors(c *C) {
 	c.Check(err, Equals, fakeError)
 
 	// Failed replies should not record notice
-	s.checkNewNotices(c, []string{})
+	s.checkNewNoticesSimple(c, []string{}, nil)
 }
 
 func (s *requestpromptsSuite) TestHandleNewRuleAllowPermissions(c *C) {
@@ -438,7 +485,7 @@ func (s *requestpromptsSuite) TestHandleNewRuleAllowPermissions(c *C) {
 	prompt4, merged := pdb.AddOrMerge(metadata, path, permissions, listenerReq4)
 	c.Check(merged, Equals, false)
 
-	s.checkNewNotices(c, []string{prompt1.ID, prompt2.ID, prompt3.ID, prompt4.ID})
+	s.checkNewNoticesSimple(c, []string{prompt1.ID, prompt2.ID, prompt3.ID, prompt4.ID}, nil)
 
 	stored := pdb.Prompts(metadata.User)
 	c.Assert(stored, HasLen, 4)
@@ -459,7 +506,11 @@ func (s *requestpromptsSuite) TestHandleNewRuleAllowPermissions(c *C) {
 
 	// Read and write permissions of prompt1 satisfied, so notice re-issued,
 	// but it has one remaining permission. prompt2 and prompt3 fully satisfied.
-	s.checkNewNoticesUnordered(c, []string{prompt1.ID, prompt2.ID, prompt3.ID})
+	e1 := &noticeInfo{promptID: prompt1.ID, data: nil}
+	e2 := &noticeInfo{promptID: prompt2.ID, data: map[string]string{"resolved": "satisfied"}}
+	e3 := &noticeInfo{promptID: prompt3.ID, data: map[string]string{"resolved": "satisfied"}}
+	expectedNotices := noticeList{e1, e2, e3}
+	s.checkNewNoticesUnordered(c, expectedNotices)
 
 	for i := 0; i < 2; i++ {
 		satisfiedReq, result, err := s.waitForListenerReqAndReply(c, listenerReqChan, replyChan)
@@ -487,7 +538,8 @@ func (s *requestpromptsSuite) TestHandleNewRuleAllowPermissions(c *C) {
 	c.Check(satisfied, HasLen, 1)
 	c.Check(satisfied[0], Equals, prompt1.ID)
 
-	s.checkNewNotices(c, []string{prompt1.ID})
+	expectedData := map[string]string{"resolved": "satisfied"}
+	s.checkNewNoticesSimple(c, []string{prompt1.ID}, expectedData)
 
 	satisfiedReq, result, err := s.waitForListenerReqAndReply(c, listenerReqChan, replyChan)
 	c.Check(err, IsNil)
@@ -536,7 +588,7 @@ func (s *requestpromptsSuite) TestHandleNewRuleDenyPermissions(c *C) {
 	prompt4, merged := pdb.AddOrMerge(metadata, path, permissions, listenerReq4)
 	c.Check(merged, Equals, false)
 
-	s.checkNewNotices(c, []string{prompt1.ID, prompt2.ID, prompt3.ID, prompt4.ID})
+	s.checkNewNoticesSimple(c, []string{prompt1.ID, prompt2.ID, prompt3.ID, prompt4.ID}, nil)
 
 	stored := pdb.Prompts(metadata.User)
 	c.Assert(stored, HasLen, 4)
@@ -557,7 +609,8 @@ func (s *requestpromptsSuite) TestHandleNewRuleDenyPermissions(c *C) {
 	c.Check(strutil.ListContains(satisfied, prompt2.ID), Equals, true)
 	c.Check(strutil.ListContains(satisfied, prompt3.ID), Equals, true)
 
-	s.checkNewNoticesUnordered(c, []string{prompt1.ID, prompt2.ID, prompt3.ID})
+	expectedData := map[string]string{"resolved": "satisfied"}
+	s.checkNewNoticesUnorderedSimple(c, []string{prompt1.ID, prompt2.ID, prompt3.ID}, expectedData)
 
 	for i := 0; i < 3; i++ {
 		satisfiedReq, result, err := s.waitForListenerReqAndReply(c, listenerReqChan, replyChan)
@@ -600,7 +653,7 @@ func (s *requestpromptsSuite) TestHandleNewRuleNonMatches(c *C) {
 	prompt, merged := pdb.AddOrMerge(metadata, path, permissions, listenerReq)
 	c.Check(merged, Equals, false)
 
-	s.checkNewNotices(c, []string{prompt.ID})
+	s.checkNewNoticesSimple(c, []string{prompt.ID}, nil)
 
 	pathPattern := "/home/test/Documents/**"
 	constraints := &prompting.Constraints{
@@ -632,7 +685,7 @@ func (s *requestpromptsSuite) TestHandleNewRuleNonMatches(c *C) {
 	c.Check(err, ErrorMatches, `internal error: invalid outcome.*`)
 	c.Check(satisfied, HasLen, 0)
 
-	s.checkNewNotices(c, []string{})
+	s.checkNewNoticesSimple(c, []string{}, nil)
 
 	otherUserMetadata := &prompting.Metadata{
 		User:      otherUser,
@@ -643,7 +696,7 @@ func (s *requestpromptsSuite) TestHandleNewRuleNonMatches(c *C) {
 	c.Check(err, IsNil)
 	c.Check(satisfied, HasLen, 0)
 
-	s.checkNewNotices(c, []string{})
+	s.checkNewNoticesSimple(c, []string{}, nil)
 
 	otherSnapMetadata := &prompting.Metadata{
 		User:      user,
@@ -654,7 +707,7 @@ func (s *requestpromptsSuite) TestHandleNewRuleNonMatches(c *C) {
 	c.Check(err, IsNil)
 	c.Check(satisfied, HasLen, 0)
 
-	s.checkNewNotices(c, []string{})
+	s.checkNewNoticesSimple(c, []string{}, nil)
 
 	otherInterfaceMetadata := &prompting.Metadata{
 		User:      user,
@@ -665,25 +718,26 @@ func (s *requestpromptsSuite) TestHandleNewRuleNonMatches(c *C) {
 	c.Check(err, IsNil)
 	c.Check(satisfied, HasLen, 0)
 
-	s.checkNewNotices(c, []string{})
+	s.checkNewNoticesSimple(c, []string{}, nil)
 
 	satisfied, err = pdb.HandleNewRule(metadata, otherConstraints, outcome)
 	c.Check(err, IsNil)
 	c.Check(satisfied, HasLen, 0)
 
-	s.checkNewNotices(c, []string{})
+	s.checkNewNoticesSimple(c, []string{}, nil)
 
 	satisfied, err = pdb.HandleNewRule(metadata, badConstraints, outcome)
 	c.Check(err, ErrorMatches, "syntax error in pattern")
 	c.Check(satisfied, HasLen, 0)
 
-	s.checkNewNotices(c, []string{})
+	s.checkNewNoticesSimple(c, []string{}, nil)
 
 	satisfied, err = pdb.HandleNewRule(metadata, constraints, outcome)
 	c.Check(err, IsNil)
 	c.Assert(satisfied, HasLen, 1)
 
-	s.checkNewNotices(c, []string{prompt.ID})
+	expectedData := map[string]string{"resolved": "satisfied"}
+	s.checkNewNoticesSimple(c, []string{prompt.ID}, expectedData)
 
 	satisfiedReq, result, err := s.waitForListenerReqAndReply(c, listenerReqChan, replyChan)
 	c.Check(err, IsNil)
@@ -733,12 +787,13 @@ func (s *requestpromptsSuite) TestClose(c *C) {
 	c.Check(pdb.MaxID(), Equals, uint64(3))
 
 	// One notice for each prompt when created
-	s.checkNewNotices(c, expectedPromptIDs)
+	s.checkNewNoticesSimple(c, expectedPromptIDs, nil)
 
 	pdb.Close()
 
 	// Once notice for each prompt when cleaned up
-	s.checkNewNoticesUnordered(c, expectedPromptIDs)
+	expectedData := map[string]string{"resolved": "cancelled"}
+	s.checkNewNoticesUnorderedSimple(c, expectedPromptIDs, expectedData)
 
 	// All prompts have been cleared, and all per-user maps deleted
 	c.Check(pdb.PerUser(), HasLen, 0)
